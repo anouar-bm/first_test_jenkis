5e0abdd3a60ec431ceb2aadc7e5747704de3a0cf
anouarbm <anouarbnmoussa@gmail.com>
2025-12-18 09:58:08 +0100

    3 test

--- POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "Hello from new test - bm 2)";
+        return "Hello from new test - bm 3)";
     }
 
     @GetMapping("/user")
